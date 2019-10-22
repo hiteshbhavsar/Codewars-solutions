95216c23246709e7ed89ece17784cf10ea4db53c
hiteshbhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2019-10-22 16:43:07 -0700

    Create Square.java
    Checking if no is square using binary search

---
+++ Square.java
@@ -0,0 +1,24 @@
+public class Square {    
+    public static boolean isSquare(int n) {        
+     if  (n  ==  0)
+       return true;
+     if  (n  <  0)
+        return false;
+    if  (n  ==  1)
+        return true;
+    long l = 1;
+    long  r = n;
+    while (l <= r) {
+        long  mid = l + (r - l )/2;
+        long  square  =  mid * mid;
+        if (square == n) {
+            return true;
+        } else if (square > n) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return false;
+    }
+}
